8fc004c351fc7bae727e1f8e4fd1d114e7e6f49f
sakshikhurana <sakshikhurana888@gmail.com>
2020-06-17 16:49:51 +0530

    Files updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
-
 urlpatterns = [
                 path('about', views.AboutView.as_view(), name='about'),
-                path('', views.DraftListView.as_view(), name='post_draft_list'),
+                path('', views.PostListView.as_view(), name='post_list'),
+                path('post/draft/', views.DraftListView.as_view(), name='post_draft_list'),
                 path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
                 path('post/new', views.CreatePostView.as_view(), name='post_new'),
                 path('post/<int:pk>/edit', views.PostUpdateView.as_view(), name='post_edit'),
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('account/login', views.login, name='login'),
-    path('account/logout', views.logout, name='logout', kwargs={'next_page':' /'}),
+    path('account/login/', LoginView.as_view(), name='login',  kwargs={'template_name':'registration/login.html'}),
+    path('account/logout', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 
 ]
